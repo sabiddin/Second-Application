86c04bf28416969d6407a4de1002b13706d234a2
sabiddin <abiddinsyed@gmail.com>
2018-02-28 01:38:56 -0500

    Second App: Added a message.

---
+++ Second-Application/Second-Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Again");
+            Console.Read();
+        }
+    }
+}
